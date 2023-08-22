13a57693c1dd5302a02187979df1f22ca7f1500c
CLAUDIO RIBEIRO DE SOUSA JUNIOR <021.888457@senaicimatec.edu.br>
2023-08-22 16:25:33 -0300

    Add youtube studio header

--- src/pages/your-videos/index.tsx
+++ src/pages/your-videos/index.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-import { MainContainer } from "../../AppStyles";
+import { MainContainer, RoutesContainer } from "../../AppStyles";
 import HeaderStudio from "../../components/header-studio";
 import MenuStudio from "../../components/menu-studio";
+import { FilterContainer, H1Title, Icon, NavContainer } from "./styles";
+import FilterIcon from "../../assets/filter.png"
+
+const firstItems = [
+  { label: "Videos" },
+  { label: "Live" },
+  { label: "Playlists" },
+  { label: "Podcasts" },
+  { label: "Promotions" },
+];
 
 function YourVideos() {
   return (
     <>
       <HeaderStudio />
       <MainContainer>
-          <MenuStudio />
+        <MenuStudio />
+        <RoutesContainer>
+          <H1Title>Channel content</H1Title>
+          <NavContainer>
+            {firstItems.map((menuItem) => (
+              <span>{menuItem.label}</span>
+            ))}
+          </NavContainer>
+          <FilterContainer>
+            <Icon alt="" src={FilterIcon} />
+            <span>Filter</span>
+          </FilterContainer>
+        </RoutesContainer>
       </MainContainer>
     </>
   );
--- src/pages/your-videos/styles.ts
+++ src/pages/your-videos/styles.ts
@@ -1,2 +1,41 @@
 import styled from "styled-components";
 
+export const H1Title = styled.h1`
+  font-size: 22px;
+  padding: 12px 0 0 32px;
+`;
+
+export const NavContainer = styled.nav`
+  position: sticky;
+  width: 100%;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: flex-start;
+
+  span {
+    color: #666;
+    font-size: 14px;
+    margin: 18px 16px 0 34px;
+    padding-bottom: 15px;
+    font-weight: 600;
+  }
+`;
+
+export const FilterContainer = styled.div`
+  position: sticky;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+  span {
+    color: #666;
+  }
+`;
+
+export const Icon = styled.img`
+  max-width: 16px;
+  padding: 10px;
+  margin: 4px 24px 4px 20px;
+`;
