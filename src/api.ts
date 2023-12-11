23190a962a688cdbc3bba657f8764a5d8eec30da
Cloviski <claudioribeiro.j@hotmail.com>
2023-12-11 14:13:58 -0300

    Revert url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://youtube-replica-green.vercel.app",
+  baseURL: "http://localhost:4000",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Context-Type": "application/json",
