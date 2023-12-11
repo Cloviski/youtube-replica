b03956f7041a98d9211e201c61312155ec76d7e8
Cloviski <claudioribeiro.j@hotmail.com>
2023-12-11 13:57:30 -0300

    Changed axios url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://youtube-replica-green.vercel.app",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Context-Type": "application/json",
