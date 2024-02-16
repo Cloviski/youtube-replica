c2f2de721a96a2b016f968e2733ada340bae8d64
Cloviski <claudioribeiro.j@hotmail.com>
2024-02-16 17:26:01 -0300

    Cypress configured

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
       bundler: "webpack",
     },
   },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
